3b183745f33e122ccaabd81083a5426549fb7a51
brendanpayne <payneb2@mail.uc.edu>
2023-10-04 20:47:41 -0400

    started work on html

--- src/main/java/edu/uc/cech/soit/myclassjournal/MyclassjournalApplication.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/MyclassjournalApplication.java
@@ -2,8 +2,9 @@ package edu.uc.cech.soit.myclassjournal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MyclassjournalApplication {
 
     public static void main(String[] args) {
